d89e6429a2cd7129a4df848713bbf2e0a819a085
Roxanne <roxanne-m@users.noreply.github.com>
2021-01-29 11:35:40 -0800

    debugging

--- src/app.js
+++ src/app.js
@@ -18,20 +18,19 @@ app.use(cors());
 
 app.use('/api/products', productsRouter);
 
-
 app.get('/', (req, res) => {
   res.send('Hello, Happy Skin Planner user!');
 });
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
+  // if (NODE_ENV === 'production') {
+  //   response = { error: { message: 'server error' } };
+  // } else {
+  console.error(error);
+  // response = { message: error.message, error };
+  // }
+  // res.status(500).json(response);
 });
 
 module.exports = app;
