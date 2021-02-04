01782d5300da9fa577480122b69e73c5c8f3ef22
Roxanne <roxanne-m@users.noreply.github.com>
2021-02-04 11:20:11 -0800

    removing console logs

--- src/app.js
+++ src/app.js
@@ -37,7 +37,6 @@ app.get('/api/weekly-planner/:week_id', (req, res) => {
   productsService
     .getWeekDayUse(req.app.get('db'), req.params.week_id)
     .then((data) => {
-      console.log(data, 'SERVER DATA');
       res.status(200).json(data);
     })
     .catch();
